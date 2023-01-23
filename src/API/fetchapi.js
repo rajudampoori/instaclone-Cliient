3b382a777a1c3f3895a2f7b498244e54c697803b
raju <rajudampoori@gmail.com>
2023-01-23 07:08:37 +0530

    error fixed

--- src/API/fetchapi.js
+++ src/API/fetchapi.js
@@ -3,9 +3,9 @@ import { useState,useEffect } from "react";
 import './card.css'
 import Header from "../header/header";
 
-
 const Api = ()=> {
     const [data,setdata] = useState([])
+
     const apiGet = ()=> {
         const httpoptions = {
         method : "GET",
@@ -16,17 +16,17 @@ const Api = ()=> {
         }
         fetch('https://instaclone-server-i6wb.onrender.com/posts',httpoptions)
         .then((data) => data.json()).then((data)=> {
-            console.log(data)
+            console.log(data)//
             setdata(data.posts.reverse())
         })
     }
+
     useEffect(()=> {
         apiGet();
     },[])
 return (
     <div>
         <Header/>
-
       {data.map((item,i)=> {
         return (
             <section key={i} className="card">
@@ -51,7 +51,7 @@ return (
                     <i class="fa fa-paper-plane-o" aria-hidden="true"></i>
                 </span>
                 <span id="date">
-                    {item.date.split('T')[1]}
+                    {item.date.split('T')[0]}
                 </span>
             </section>
             <section className='likes'>
